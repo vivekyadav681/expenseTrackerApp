229291c0f80976f52adfa73e741fc057b794651c
Vivek Yadav <vivekyadav00681@gmail.com>
2025-09-23 22:43:14 +0530

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,40 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'something',
+      amount: 100,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: 'Something else',
+      amount: 120,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Some other shitty things',
+      amount: 200,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: const [Text('Something'), Text('Something else')]),
+    );
+  }
+}
---
+++ lib/models/expense_list.dart
@@ -0,0 +1,12 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseList extends StatelessWidget {
+  const ExpenseList({super.key, required expenses});
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
