2253d58c8518bf513796012e7984c6845b9eb540
Vivek Yadav <vivekyadav00681@gmail.com>
2025-09-25 11:10:48 +0530

    init commit

--- lib/models/expense_list.dart
+++ lib/models/expense_list.dart
@@ -2,7 +2,7 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseList extends StatelessWidget {
-  const ExpenseList({super.key, required expenses});
+  const ExpenseList({super.key, required this.expenses});
   final List<Expense> expenses;
 
   @override
