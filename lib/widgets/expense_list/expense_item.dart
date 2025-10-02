26b962fac481574e5eab6cce64c4edbf0258e1c6
Vivek Yadav <vivekyadav00681@gmail.com>
2025-10-02 13:40:45 +0530

    ...

--- lib/widgets/expense_list/expense_item.dart
+++ lib/widgets/expense_list/expense_item.dart
@@ -1,11 +1,36 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseItem extends StatelessWidget {
-  const ExpenseItem({super.key});
-
+  const ExpenseItem(this.expense, {super.key});
+  final Expense expense;
 
   @override
   Widget build(BuildContext context) {
-    return Text('something');
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    SizedBox(width: 8),
+                    //Text(expense.category.toString()),
+                    Text(expense.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/expense_list/expense_list.dart
+++ lib/widgets/expense_list/expense_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expense_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseList extends StatelessWidget {
@@ -9,7 +10,7 @@ class ExpenseList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Text(expenses[index].title),
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
     );
   }
 }
