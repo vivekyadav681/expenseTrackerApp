4561ebc33c324ee25fcffd8278d0cd451180a27e
Vivek Yadav <vivekyadav00681@gmail.com>
2025-10-09 21:54:46 +0530

    ...

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,8 +10,15 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-
   final _textController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
 
   // String _enteredTitle = "";
   // void _saveTitle(String value) {
@@ -29,16 +36,32 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text("Title: ")),
           ),
+          TextField(
+            controller: _amountController,
+            maxLength: 4,
+            decoration: InputDecoration(
+              label: Text("Amount"),
+              prefixText: '\$ ',
+            ),
+            keyboardType: TextInputType.number,
+          ),
           Row(
             children: [
-              ElevatedButton(onPressed: () {
-              }, child: Text("Something"))
+              Container(
+                margin: EdgeInsets.all(5),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print(_textController.text);
+                    print(_amountController.text);
+                  },
+                  child: Text("Save Expense"),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(5),
+                child: ElevatedButton(onPressed: () {}, child: Text("Cancel")),
+              ),
             ],
-          ),
-          TextField(
-            maxLength: 4,
-            decoration: InputDecoration(label: Text("Amount")),
-            keyboardType: TextInputType.number,
           ),
         ],
       ),
