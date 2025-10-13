371fba07744f6c05c8446df42017b59cd13739ff
Vivek Yadav <vivekyadav00681@gmail.com>
2025-10-13 14:09:29 +0530

    added a dropdown button to the modal screen

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+var formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -12,6 +17,22 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _textController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category selectedCategory = Category.food;
+
+  void _openDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -36,17 +57,64 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text("Title: ")),
           ),
-          TextField(
-            controller: _amountController,
-            maxLength: 4,
-            decoration: InputDecoration(
-              label: Text("Amount"),
-              prefixText: '\$ ',
-            ),
-            keyboardType: TextInputType.number,
-          ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  maxLength: 4,
+                  decoration: InputDecoration(
+                    label: Text("Amount"),
+                    prefixText: '\$ ',
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'no date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _openDatePicker,
+                      icon: Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.all(5),
+                child: DropdownButton(
+                  padding: EdgeInsets.all(1),
+                  value: selectedCategory,
+                  items: Category.values
+                      .map(
+                        (e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(e.name.toUpperCase()),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) return;
+                    setState(() {
+                      selectedCategory = value;
+                    });
+                  },
+                ),
+              ),
+              const Spacer(),
               Container(
                 margin: EdgeInsets.all(5),
                 child: ElevatedButton(
@@ -59,7 +127,12 @@ class _NewExpenseState extends State<NewExpense> {
               ),
               Container(
                 margin: EdgeInsets.all(5),
-                child: ElevatedButton(onPressed: () {}, child: Text("Cancel")),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Cancel"),
+                ),
               ),
             ],
           ),
